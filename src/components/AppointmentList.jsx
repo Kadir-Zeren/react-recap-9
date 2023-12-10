f8385d49816f9b45aa3ccb5f6f8b95391aec74c0
kadir-zeren <k4dzeren@gmail.com>
2023-12-10 21:41:08 +0300

    feat: appointment list added

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -1,13 +1,35 @@
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { TiDelete } from "react-icons/ti";
 
-const AppointmentList = () => {
+const AppointmentList = ({ apps }) => {
+  console.log(apps);
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
         Appointment List
       </h3>
-    </Container>
-  )
-}
 
-export default AppointmentList
+      {apps.map(({ id, patient, consulted, doctor, day }) => (
+        <div
+          key={id}
+          className={consulted ? "appointments consulted" : "appointments"}
+        >
+          <Row>
+            <Col>
+              <h4>{patient}</h4>
+              <h5>{doctor}</h5>
+            </Col>
+            <Col>{day}</Col>
+            <Col>
+              <TiDelete className="text-danger fs-1" />
+            </Col>
+          </Row>
+        </div>
+      ))}
+    </Container>
+  );
+};
+
+export default AppointmentList;
